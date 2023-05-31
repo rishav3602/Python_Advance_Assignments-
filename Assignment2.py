ad18eead7c3d3eb2da6d501c8b0f8a5349f699ec
Rishav Kumar <113835701+rishav3602@users.noreply.github.com>
2023-05-31 14:26:44 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,30 +1,49 @@
 """
 Q1. What is the relationship between classes and modules?
-Ans - In Python, a module is a file that contains Python code, while a class is a blueprint for creating objects. Classes can be defined within modules, and modules can contain multiple classes. Modules provide a way to organize and reuse code, and classes within modules allow for encapsulation of related functionality.
+Ans - In Python, a module is a file that contains Python code, while a class is a blueprint for creating objects.
+Classes can be defined within modules, and modules can contain multiple classes. Modules provide a way to organize
+and reuse code, and classes within modules allow for encapsulation of related functionality.
 
 Q2. How do you make instances and classes?
-Ans - To create an instance of a class, you simply call the class as if it were a function. For example, if the class is named MyClass, you can create an instance by writing my_instance = MyClass(). To create a class, you use the class keyword followed by the name of the class and define its attributes and methods within the class block.
+Ans - To create an instance of a class, you simply call the class as if it were a function. For example, if the class
+is named MyClass, you can create an instance by writing my_instance = MyClass(). To create a class, you use the class
+keyword followed by the name of the class and define its attributes and methods within the class block.
 
 Q3. Where and how should class attributes be created?
-Ans - Class attributes should be created inside the class block, but outside of any methods. They are usually defined directly below the class name and above any methods. Class attributes are shared among all instances of the class and can be accessed using the class name or any instance of the class.
+Ans - Class attributes should be created inside the class block, but outside of any methods. They are usually defined
+directly below the class name and above any methods. Class attributes are shared among all instances of the class and
+can be accessed using the class name or any instance of the class.
+
 
 Q4. Where and how are instance attributes created?
-Ans - Instance attributes are created inside the __init__ method of a class. Each instance of the class will have its own copy of the instance attributes. Inside the __init__ method, you assign values to the instance attributes using the self keyword, which refers to the instance being created.
+Ans - Instance attributes are created inside the __init__ method of a class. Each instance of the class will have its
+own copy of the instance attributes. Inside the __init__ method, you assign values to the instance attributes using
+the self keyword, which refers to the instance being created.
 
 Q5. What does the term "self" in a Python class mean?
-Ans - In Python, the term "self" refers to the instance of a class. It is a convention to name the first parameter of instance methods as self. When a method is called on an instance, the self parameter allows the method to access and manipulate the attributes and methods specific to that instance.
+Ans - In Python, the term "self" refers to the instance of a class. It is a convention to name the first parameter of
+instance methods as self. When a method is called on an instance, the self parameter allows the method to access and
+manipulate the attributes and methods specific to that instance.
 
 Q6. How does a Python class handle operator overloading?
-Ans - Python classes can handle operator overloading by defining special methods that correspond to specific operators. For example, the + operator can be overloaded by defining the __add__ method in a class. These special methods allow instances of the class to behave like built-in Python types and support operations such as addition, subtraction, comparison, and more.
+Ans - Python classes can handle operator overloading by defining special methods that correspond to specific operators.
+For example, the + operator can be overloaded by defining the __add__ method in a class. These special methods allow
+instances of the class to behave like built-in Python types and support operations such as addition, subtraction, comparison, and more.
 
 Q7. When do you consider allowing operator overloading of your classes?
-Ans - Operator overloading should be considered when it makes sense for the objects of a class to support common operators in a meaningful way. It is useful when you want to provide intuitive behavior for operators specific to your custom class, allowing instances to be used in expressions and operations similar to built-in types.
+Ans - Operator overloading should be considered when it makes sense for the objects of a class to support common
+operators in a meaningful way. It is useful when you want to provide intuitive behavior for operators specific
+to your custom class, allowing instances to be used in expressions and operations similar to built-in types.
 
 Q8. What is the most popular form of operator overloading?
-Ans - The most popular form of operator overloading in Python is the arithmetic operator overloading, such as +, -, *, /, etc. This allows instances of a class to perform arithmetic operations based on the custom implementation defined in the class.
+Ans - The most popular form of operator overloading in Python is the arithmetic operator overloading,
+such as +, -, *, /, etc. This allows instances of a class to perform arithmetic operations based on the 
+custom implementation defined in the class.
 
 Q9. What are the two most important concepts to grasp in order to comprehend Python OOP code?
-Ans - The two most important concepts to grasp in order to comprehend Python object-oriented programming (OOP) code are classes and objects. Understanding how classes define the structure and behavior of objects and how objects interact with each other and the outside world is crucial in working with Python's OOP paradigm.
+Ans - The two most important concepts to grasp in order to comprehend Python object-oriented programming
+(OOP) code are classes and objects. Understanding how classes define the structure and behavior of objects
+and how objects interact with each other and the outside world is crucial in working with Python's OOP paradigm.
 
 
 
