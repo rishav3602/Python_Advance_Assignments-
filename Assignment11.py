5aa0bdd7f74f78b1847b9cadcf85c934f6bb19a5
Rishav Kumar <113835701+rishav3602@users.noreply.github.com>
2023-05-31 15:25:36 +0530

    Update Assignment11.py

--- Assignment11.py
+++ Assignment11.py
@@ -1,30 +1,48 @@
 """
 Q1. What is the concept of a metaclass?
 
-Ans 1. In Python, a metaclass is a class that defines the behavior and structure of other classes. It is responsible for creating and initializing classes. Metaclasses allow you to control the creation and behavior of classes at a higher level than regular class definitions. By defining a metaclass, you can customize how classes are instantiated, how attributes are added or modified, and how methods are defined and inherited.
+Ans 1. In Python, a metaclass is a class that defines the behavior and structure of other classes.
+It is responsible for creating and initializing classes. Metaclasses allow you to control the creation
+and behavior of classes at a higher level than regular class definitions. By defining a metaclass,
+you can customize how classes are instantiated, how attributes are added or modified, and how methods
+are defined and inherited.
 
 Q2. What is the best way to declare a class's metaclass?
 
-Ans 2. The best way to declare a class's metaclass in Python is by setting the __metaclass__ attribute at the top of the class definition. Here's an example:
+Ans 2. The best way to declare a class's metaclass in Python is by setting the __metaclass__ attribute
+at the top of the class definition. Here's an example:
 
 
 
 Q3. How do class decorators overlap with metaclasses for handling classes?
 
-Ans 3. Class decorators and metaclasses both provide mechanisms for modifying the behavior of classes. They overlap in the sense that they can both be used to add, modify, or remove attributes and methods of a class. However, there are some differences between them:
+Ans 3. Class decorators and metaclasses both provide mechanisms for modifying the behavior of classes.
+They overlap in the sense that they can both be used to add, modify, or remove attributes and methods
+of a class. However, there are some differences between them:
 
-Class decorators are functions or callable objects that take a class as input and return a modified version of that class. They are applied directly to the class definition using the @decorator syntax. Class decorators are executed immediately after the class definition is evaluated.
+Class decorators are functions or callable objects that take a class as input and return a modified version
+of that class. They are applied directly to the class definition using the @decorator syntax. Class decorators
+are executed immediately after the class definition is evaluated.
 
-Metaclasses, on the other hand, are used to define the behavior and structure of classes at a higher level. They operate at the time of class creation and are responsible for initializing and constructing classes. Metaclasses are specified by setting the metaclass argument in the class definition or by using the __metaclass__ attribute.
+Metaclasses, on the other hand, are used to define the behavior and structure of classes at a higher level.
+They operate at the time of class creation and are responsible for initializing and constructing classes.
+Metaclasses are specified by setting the metaclass argument in the class definition or by using the __metaclass__ attribute.
 
-While class decorators and metaclasses can achieve similar results, the main difference is in the level at which they operate. Class decorators focus on modifying individual classes, while metaclasses provide a higher-level control over the creation and behavior of classes.
+While class decorators and metaclasses can achieve similar results, the main difference is in the level
+at which they operate. Class decorators focus on modifying individual classes, while metaclasses provide
+a higher-level control over the creation and behavior of classes.
 
 Q4. How do class decorators overlap with metaclasses for handling instances?
 
-Ans 4. Class decorators and metaclasses overlap in terms of modifying instances of classes indirectly. While both can be used to customize the behavior of instances, the approaches differ:
+Ans 4. Class decorators and metaclasses overlap in terms of modifying instances of classes indirectly.
+While both can be used to customize the behavior of instances, the approaches differ:
 
-Class decorators modify instances by adding, modifying, or wrapping methods and attributes of the class. These modifications affect all instances of the class, as the class definition itself is modified.
+Class decorators modify instances by adding, modifying, or wrapping methods and attributes of the class.
+These modifications affect all instances of the class, as the class definition itself is modified.
 
-Metaclasses can also indirectly modify instances by controlling the creation and initialization process of the class. By customizing the __new__() and __init__() methods of the metaclass, you can customize how instances of the class are created and initialized. However, metaclasses have a broader scope and can also influence class-level behavior.
+Metaclasses can also indirectly modify instances by controlling the creation and initialization process 
+of the class. By customizing the __new__() and __init__() methods of the metaclass, you can customize how
+instances of the class are created and initialized. However, metaclasses have a broader scope and can also 
+influence class-level behavior.
 
 """
