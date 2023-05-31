bc3551ddfe67969b833ba61572492b9613f2c303
Rishav Kumar <113835701+rishav3602@users.noreply.github.com>
2023-05-31 14:21:46 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -24,20 +24,26 @@ The superclasses of each superclass, following the same order.
 """
 Q3. How do you distinguish between a class object and an instance object?
 
-Ans- A class object is the blueprint or template for creating instances of that class. An instance object, on the other hand, is a specific object created from a class, representing a unique instance of that class.
+Ans- A class object is the blueprint or template for creating instances of that class.
+An instance object, on the other hand, is a specific object created from a class,
+representing a unique instance of that class.
 
 """
 
 """
 Q4. What makes the first argument in a class’s method function special?
 
-Ans- The first argument in a class's method function, conventionally named self, refers to the instance of the class that the method is being called on. It allows the method to access and manipulate the instance's attributes and methods.
+Ans- The first argument in a class's method function, conventionally named self,
+refers to the instance of the class that the method is being called on. It allows
+the method to access and manipulate the instance's attributes and methods.
 
 """
 """
 Q5. What is the purpose of the __init__ method?
 
-Ans- The __init__ method is a special method in Python classes that is called automatically when a new instance of the class is created. It is used to initialize the attributes of the instance and perform any other setup actions that are required before the instance can be used.
+Ans- The __init__ method is a special method in Python classes that is called automatically
+when a new instance of the class is created. It is used to initialize the attributes of the
+instance and perform any other setup actions that are required before the instance can be used.
 
 """
 """
@@ -50,13 +56,18 @@ Ans- To create a class instance, you simply call the class as if it were a funct
 """
 Q7. What is the process for creating a class?
 
-Ans- To create a class in Python, you use the class keyword followed by the name of the class. Inside the class, you define attributes and methods that describe the behavior and characteristics of the class.
+Ans- To create a class in Python, you use the class keyword followed by the name of the class.
+Inside the class, you define attributes and methods that describe the behavior and characteristics of the class.
 
 """
 
 """
 Q8. How would you define the superclasses of a class?
 
-Ans- The superclasses of a class, also known as parent classes or base classes, are the classes from which the current class inherits. In Python, you define the superclasses by including them in the parentheses after the class name during class creation. For example, if a class SubClass inherits from a class SuperClass, the definition would be class SubClass(SuperClass). If there are multiple superclasses, they can be separated by commas.
+Ans- The superclasses of a class, also known as parent classes or base classes, are the classes
+from which the current class inherits. In Python, you define the superclasses by including them
+in the parentheses after the class name during class creation. For example, if a class SubClass
+inherits from a class SuperClass, the definition would be class SubClass(SuperClass). If there
+are multiple superclasses, they can be separated by commas.
 
 """
